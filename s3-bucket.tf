0ff90c00f6482ea8a0d509e3de76c59f962515c0
chuhann <chuhan8099@hotmail.com>
2023-02-17 15:39:12 +0800

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,4 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-  bucket_prefix="ch"
-
-  versioning = {
-    enabled = true
-  }
-
+module "s3-bucket" {
+  source  = "app.terraform.io/chworkspace/s3-bucket/aws"
+  version = "2.8.0"
 }
